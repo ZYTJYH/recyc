0d33057a412e71cddf2c88e1683596db9a1bf342
zytjyh <793360641@qq.com>
2018-11-13 13:59:15 +0800

    十月13号新增 接下来的事情： 1.扩展数据库 2.修改接口

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,65 @@
 -- Positions表测试数据
-INSERT INTO Positions(employee_id,time,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000001","2018-01-02 00:00:00",114.980,32.7435,'on',"1000000001","一号先生 18756929625",'car');
-INSERT INTO Positions(employee_id,time,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000002","2018-01-02 00:00:00",114.987,32.754,'on',"1000000001","二号先生 18756929625",'bin');
-INSERT INTO Positions(employee_id,time,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000003","2018-01-02 00:00:00",114.974,32.7435,'on',"1000000001","三号先生 18756929625",'person');
 
+
+
+INSERT INTO Positions(employee_id,`time`,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000001","2018-01-02 00:00:00",114.990629,32.757884,'on',"1000000001","1号员工 18756929625",'car');
+INSERT INTO Positions(employee_id,`time`,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000002","2018-01-02 00:00:00",114.987,32.754,'on',"1000000001","2号员工 18756929625",'person');
+INSERT INTO Positions(employee_id,`time`,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000003","2018-01-02 00:00:00",114.985172,32.750096,'on',"1000000001","3号员工 18756929625",'person');
+INSERT INTO Positions(employee_id,`time`,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000004","2018-01-02 00:00:00",114.990629,32.757884,'on',"1000000001","4号员工 18756929625",'person');
+INSERT INTO Positions(employee_id,`time`,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000005","2018-01-02 00:00:00",114.985172,32.750096,'on',"1000000001","5号先生 18756929625",'car');
+INSERT INTO Positions(employee_id,`time`,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000006","2018-01-02 00:00:00",114.990629,32.757884,'on',"1000000001","6号员工 18756929625",'person');
+INSERT INTO Positions(employee_id,`time`,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000007","2018-01-02 00:00:00",114.985172,32.750096,'on',"1000000001","7号先生 18756929625",'person');
+INSERT INTO Positions(employee_id,`time`,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000008","2018-01-02 00:00:00",114.990629,32.757884,'on',"1000000001","8号先生 18756929625",'person');
+INSERT INTO Positions(employee_id,`time`,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000009","2018-01-02 00:00:00",114.985172,32.750096,'on',"1000000001","9号先生 18756929625",'person');
+INSERT INTO Positions(employee_id,`time`,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000010","2018-01-02 00:00:00",114.990629,32.757884,'on',"1000000001","10号先生 18756929625",'person');
+INSERT INTO Positions(employee_id,`time`,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000011","2018-01-02 00:00:00",114.985172,32.750096,'on',"1000000001","11号先生 18756929625",'person');
+INSERT INTO Positions(employee_id,`time`,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000012","2018-01-02 00:00:00",114.990629,32.757884,'on',"1000000001","12号先生 18756929625",'person');
+INSERT INTO Positions(employee_id,`time`,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000013","2018-01-02 00:00:00",114.985172,32.750096,'on',"1000000001","5号先生 18756929625",'car');
+INSERT INTO Positions(employee_id,`time`,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000014","2018-01-02 00:00:00",114.990629,32.757884,'on',"1000000001","6号先生 18756929625",'person');
+INSERT INTO Positions(employee_id,`time`,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000015","2018-01-02 00:00:00",114.985172,32.750096,'on',"1000000001","7号先生 18756929625",'person');
+INSERT INTO Positions(employee_id,`time`,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000016","2018-01-02 00:00:00",114.990629,32.757884,'on',"1000000001","8号先生 18756929625",'car');
+INSERT INTO Positions(employee_id,`time`,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000017","2018-01-02 00:00:00",114.985172,32.750096,'on',"1000000001","9号先生 18756929625",'person');
+INSERT INTO Positions(employee_id,`time`,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000018","2018-01-02 00:00:00",114.990629,32.757884,'on',"1000000001","10号先生 18756929625",'person');
+INSERT INTO Positions(employee_id,`time`,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000019","2018-01-02 00:00:00",114.985172,32.750096,'on',"1000000001","11号先生 18756929625",'person');
+INSERT INTO Positions(employee_id,`time`,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000020","2018-01-02 00:00:00",114.990629,32.757884,'on',"1000000001","4号先生 18756929625",'person');
+INSERT INTO Positions(employee_id,`time`,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000021","2018-01-02 00:00:00",114.985172,32.750096,'on',"1000000001","5号先生 18756929625",'car');
+INSERT INTO Positions(employee_id,`time`,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000022","2018-01-02 00:00:00",114.990629,32.757884,'on',"1000000001","6号先生 18756929625",'person');
+INSERT INTO Positions(employee_id,`time`,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000023","2018-01-02 00:00:00",114.985172,32.750096,'on',"1000000001","7号先生 18756929625",'person');
+INSERT INTO Positions(employee_id,`time`,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000024","2018-01-02 00:00:00",114.990629,32.757884,'on',"1000000001","8号先生 18756929625",'person');
+INSERT INTO Positions(employee_id,`time`,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000025","2018-01-02 00:00:00",114.985172,32.750096,'on',"1000000001","9号先生 18756929625",'car');
+INSERT INTO Positions(employee_id,`time`,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000026","2018-01-02 00:00:00",114.990629,32.757884,'on',"1000000001","10号先生 18756929625",'person');
+INSERT INTO Positions(employee_id,`time`,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000027","2018-01-02 00:00:00",114.985172,32.750096,'on',"1000000001","11号先生 18756929625",'person');
+INSERT INTO Positions(employee_id,`time`,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000028","2018-01-02 00:00:00",114.990629,32.757884,'on',"1000000001","4号先生 18756929625",'person');
+INSERT INTO Positions(employee_id,`time`,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000029","2018-01-02 00:00:00",114.985172,32.750096,'on',"1000000001","5号先生 18756929625",'car');
+INSERT INTO Positions(employee_id,`time`,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000030","2018-01-02 00:00:00",114.990629,32.757884,'on',"1000000001","6号先生 18756929625",'person');
+INSERT INTO Positions(employee_id,`time`,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000031","2018-01-02 00:00:00",114.985172,32.750096,'on',"1000000001","7号先生 18756929625",'person');
+INSERT INTO Positions(employee_id,`time`,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000032","2018-01-02 00:00:00",114.990629,32.757884,'on',"1000000001","8号先生 18756929625",'person');
+INSERT INTO Positions(employee_id,`time`,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000033","2018-01-02 00:00:00",114.985172,32.750096,'on',"1000000001","9号先生 18756929625",'car');
+INSERT INTO Positions(employee_id,`time`,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000034","2018-01-02 00:00:00",114.990629,32.757884,'on',"1000000001","10号先生 18756929625",'person');
+INSERT INTO Positions(employee_id,`time`,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000035","2018-01-02 00:00:00",114.985172,32.750096,'on',"1000000001","11号先生 18756929625",'person');
+INSERT INTO Positions(employee_id,`time`,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000036","2018-01-02 00:00:00",114.990629,32.757884,'on',"1000000001","4号先生 18756929625",'person');
+INSERT INTO Positions(employee_id,`time`,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000037","2018-01-02 00:00:00",114.985172,32.750096,'on',"1000000001","5号先生 18756929625",'car');
+INSERT INTO Positions(employee_id,`time`,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000038","2018-01-02 00:00:00",114.990629,32.757884,'on',"1000000001","6号先生 18756929625",'person');
+INSERT INTO Positions(employee_id,`time`,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000039","2018-01-02 00:00:00",114.985172,32.750096,'on',"1000000001","7号先生 18756929625",'person');
+INSERT INTO Positions(employee_id,`time`,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000040","2018-01-02 00:00:00",114.990629,32.757884,'on',"1000000001","8号先生 18756929625",'person');
+INSERT INTO Positions(employee_id,`time`,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000041","2018-01-02 00:00:00",114.990629,32.757884,'on',"1000000001","4号先生 18756929625",'person');
+INSERT INTO Positions(employee_id,`time`,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000042","2018-01-02 00:00:00",114.985172,32.750096,'on',"1000000001","5号先生 18756929625",'car');
+INSERT INTO Positions(employee_id,`time`,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000043","2018-01-02 00:00:00",114.990629,32.757884,'on',"1000000001","6号先生 18756929625",'person');
+INSERT INTO Positions(employee_id,`time`,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000044","2018-01-02 00:00:00",114.985172,32.750096,'on',"1000000001","7号先生 18756929625",'person');
+INSERT INTO Positions(employee_id,`time`,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000045","2018-01-02 00:00:00",114.990629,32.757884,'on',"1000000001","8号先生 18756929625",'person');
+INSERT INTO Positions(employee_id,`time`,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000046","2018-01-02 00:00:00",114.985172,32.750096,'on',"1000000001","9号先生 18756929625",'car');
+INSERT INTO Positions(employee_id,`time`,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000047","2018-01-02 00:00:00",114.990629,32.757884,'on',"1000000001","10号先生 18756929625",'person');
+INSERT INTO Positions(employee_id,`time`,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000048","2018-01-02 00:00:00",114.985172,32.750096,'on',"1000000001","11号先生 18756929625",'person');
+
+
+INSERT INTO Positions(employee_id,time,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000044","2018-01-02 00:00:00",114.985172,32.750096,'on',"1000000001"," 18756929625",'bin');
+INSERT INTO Positions(employee_id,time,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000045","2018-01-02 00:00:00",114.990629,32.757884,'on',"1000000001","8号先生 18756929625",'bin');
+INSERT INTO Positions(employee_id,time,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000046","2018-01-02 00:00:00",114.985172,32.750096,'on',"1000000001","9号先生 18756929625",'bin');
+INSERT INTO Positions(employee_id,time,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000047","2018-01-02 00:00:00",114.990629,32.757884,'on',"1000000001","10号先生 18756929625",'bin');
+INSERT INTO Positions(employee_id,time,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000048","2018-01-02 00:00:00",114.985172,32.750096,'on',"1000000001","11号先生 18756929625",'bin');
+INSERT INTO Positions(employee_id,time,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000046","2018-01-02 00:00:00",114.9,32.750096,'on',"1000000001","9号先生 18756929625",'bin');
+INSERT INTO Positions(employee_id,time,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000047","2018-01-02 00:00:00",114.990629,32.757884,'on',"1000000001","10号先生 18756929625",'bin');
+INSERT INTO Positions(employee_id,time,longitude,latitude,work_status,boss_id,information,kind) VALUES("0000000048","2018-01-02 00:00:00",114.985172,32.750096,'on',"1000000001","11号先生 18756929625",'bin');
 
 -- Bosses表测试数据
 INSERT INTO Bosses(boss_id,`name`,phone) VALUES("1000000001","王总","12233334444")
